e92daff347f503beda0fb26ca90b3e55f8bfbf4b
Formacion <formacion@FTAULAS.ORG>
2021-05-25 10:15:29 +0200

    variables

---
+++ main.js
@@ -0,0 +1,14 @@
+let empezar ="esta es mi primer variable"
+console.log(empezar);
+document.querySelector(".variable-ejemplo").innerHTML = `${empezar}`;
+
+
+empezar = 4;
+console.log(empezar);
+document.querySelector(".variable-ejemplo").innerHTML = `Ahora mi variable empezar vale: ${empezar}`
+
+/*let empezarDos = 2;
+console.log(empezarDos); 
+empezar = true; 
+/*es un error*/
+
